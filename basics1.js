9954d2cbaa83595ce6589887981b403ebd00224b
PRE4CH3R <ananthan.stvm@gmail.com>
2023-03-31 20:10:20 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,72 @@
+// a=['meera','nithya',10];
+// console.log(a[2]);
+// a.push('orange');
+// console.log(a);
+// a.pop();
+// console.log(a);
+// let person={
+//    name:'Rahul',
+//    age:'23',
+//    place:'trivandrum'
+// }
+
+
+// console.log(person);
+// var a=2;
+// a++;
+// console.log(a);
+
+
+// a=2;
+// b=2;
+// if(a>b)
+// {
+    // console.log('a is larger');
+// }
+// else if(b>a)
+// {
+    // console.log('b is large');
+// }
+// else {    console.log("same");
+// }
+
+
+// function addt(a,b){
+    // sum=a+b;
+    // return sum;
+// }
+// addt(5,5);
+//function mult(a,b){
+    // prodct=a*b;
+    // return mult;
+// }
+// mult(5,10);
+// function divs(a,b){
+    // div=a/b;
+    // return div
+// }
+// result=divs(5,25);
+// console.log(result);
+// console.log('hello world');
+
+
+// var sum=0;
+// for(let i=0;i<=10;i++)
+// { 
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+
+// i=2;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<5)
+
+
+// let classes=[1,2,3];
+// for(i of classes)
+// {
+//     console.log(`Hello ${i}`)
+// }
